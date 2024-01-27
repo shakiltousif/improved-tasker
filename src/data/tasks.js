ea50664729ab2f7c2125d004ba9aa5bbc4768913
Shakil Ahamed <shakil.ahamed.tousif.800@gmail.com>
2024-01-27 22:58:44 +0600

    First Commit

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -1,6 +1,6 @@
 const tasks = [
     {
-        id: 1,
+        id: 0,
         title: "Convert Html",
         description: "আমাদের দেয়া, HTML টেমপ্লেট এর কোড গুলো নিয়ে, নতুন একটি Vite প্রোজেক্ট initate করে সম্পুর্ন scratch থেকে এই এসাইনমেন্টটি করতে হবে ",
         tags: ["html", "css"],
@@ -9,7 +9,7 @@ const tasks = [
     },
 
     {
-        id: 2,
+        id: 1,
         title: "use Context Api and useReducer",
         description: "এই প্রজেক্টের State Management এর ক্ষেত্রে আপনাকে অবশ্যই Context API এবং useReducer ব্যবহার করতে হবে ",
         tags: ["contextapi", "usereducer", "assignment"],
@@ -18,7 +18,7 @@ const tasks = [
     },
 
     {
-        id: 3,
+        id: 2,
         title: "Make Add Task Function",
         description: 'আপনার প্রজেক্টের "Add Task" বাটনে ক্লিক করলে আপনাকে একটি popup দেখাতে হবে এবং সেই popup-এ প্রয়োজনীয় তথ্য প্রদান করে "Create New Task" বাটনে ক্লিক করলে Task List এ নতুন Task হিসেবে যুক্ত হবে । Popup তৈরির জন্যে আলাদা কোন প্যাকেজ ব্যবহার করতে পারবেন না ।',
         tags: ["react", "assignment"],
@@ -27,7 +27,7 @@ const tasks = [
     },
 
     {
-        id: 4,
+        id: 3,
         title: "Delete Task Function",
         description: "Task ডিলিট বাটনে ক্লিক করলে, Task টি Task List থেকে রিমুভ হয়ে যাবে",
         tags: ["delete", "task", "assignment"],
@@ -36,7 +36,7 @@ const tasks = [
     },
 
     {
-        id: 5,
+        id: 4,
         title: "Toggle Favourite Functionality",
         description: "Task গুলো Favourite এবং Unfavourite-এ toggle করা যাবে ।",
         tags: ["toggle", "function"],
